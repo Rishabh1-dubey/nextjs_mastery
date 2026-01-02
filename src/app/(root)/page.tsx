5f4b6c5a35b03b213be9ba2a1e4e257a2e2c06e6
Rishabh1-dubey <rishabhdubey006@gmail.com>
2026-01-02 14:41:34 +0530

    built a home section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,34 @@
+import { Button } from "@/components/ui/button";
 import { auth } from "../../../auth";
+import Link from "next/link";
+import ROUTES from "../../../constants/route";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
 
 const Home = async () => {
   const session = await auth();
   console.log(session);
   return (
-    <div>
-      <h1 className="text-3xl text-center font-semibold mb-2 ">
-        Welcome to the Next.js
-      </h1>
-    </div>
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11 relative text-center"></section>
+      HoemFIlter
+      <div className="mt-10 w-full flex-col gap-6">
+        <p>question 1</p>
+        <p>question 1</p>
+        <p>question 1</p>
+        <p>question 1</p>
+        <p>question 1</p>
+      </div>
+    </>
   );
 };
 export default Home;
